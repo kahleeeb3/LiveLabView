987ea3705d7e38942e1317f9c4c66fcca2f3d16c
kahleeeb3 <powellcalebm@gmail.com>
2024-10-28 16:52:09 -0400

    expandable grids added

--- tk_helper.py
+++ tk_helper.py
@@ -104,17 +104,20 @@ class Window:
     def create_top_frame(self):
         self.top_frame = tk.Frame(self.root, bg='light grey', width=50, height=25)
         self.top_frame.grid(row=0, sticky="nsew")
-        self.top_frame.grid_columnconfigure(0, weight=1)
+        tk.Grid.columnconfigure(self.root, 0, weight=1)
 
     def create_bottom_frame(self):
         self.bottom_frame = tk.Frame(self.root, bg='green', width=50, height=25)
         self.bottom_frame.grid(row=1, sticky="nsew")
+        tk.Grid.rowconfigure(self.root, 1, weight=1)
     
     def create_bottom_left_frame(self):
         self.bottom_left_frame = tk.Frame(self.bottom_frame, bg='light grey', width=20, height=190)
         self.bottom_left_frame.grid(row=0, column=0, sticky="nsew")
+        tk.Grid.rowconfigure(self.bottom_frame, 0, weight=1)
 
     def create_bottom_right_frame(self):
         self.bottom_right_frame = tk.Frame(self.bottom_frame, bg='white', width=250, height=190)
         self.bottom_right_frame.grid(row=0, column=1, sticky="nsew")
-        self.bottom_right_frame.grid_columnconfigure(0, weight=1)
+        tk.Grid.rowconfigure(self.bottom_frame, 0, weight=1)
+        tk.Grid.columnconfigure(self.bottom_frame, 1, weight=1)
