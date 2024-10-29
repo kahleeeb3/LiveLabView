dc6385c4a68e4f23485b651d4c265c5c36515068
kahleeeb3 <powellcalebm@gmail.com>
2024-10-29 16:18:54 -0400

    Automatic Height Adjustment

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ class Content:
     def __init__(self):
 
         self.root = tk.Tk()
+        # self.root.state('zoomed')  # Maximize the window
+        self.root.geometry("1200x400")
         self.root.iconbitmap("icon.ico")
         self.root.title("LiveLabView")
 
@@ -30,12 +32,15 @@ class Content:
     def create_window(self):
         self.window = tkh.Window(self.root)
         self.collapsable_width = 40
-        self.collapsable_height = 96
+        self.filter_bar_height = 25
     
     def create_location_selection(self):
-        location_menu = tkh.Collapsable(master=self.window.bottom_left_frame, row=0, column=0, name="Locations", width=self.collapsable_width)
-        location_scroll = tkh.ScrollFrame(location_menu.collapsed_content, width=25, height=self.collapsable_height)
 
+        # create the collapsable
+        location_menu = tkh.Collapsable(master=self.window.bottom_left_frame, row=0, column=0, name="Locations", width=self.collapsable_width)
+
+        # add content to the collapsable
+        location_scroll = tkh.ScrollFrame(location_menu.collapsed_content, width=self.collapsable_width)
         self.location_vars = {}
         for i, l in enumerate(self.locations):
             self.location_vars[l] = tk.BooleanVar()
@@ -44,8 +49,12 @@ class Content:
         location_scroll.pack(side="top", fill="both", expand=True)
 
     def create_instructor_selection(self):
+        
+        # create the collapsable
         instructor_menu = tkh.Collapsable(master=self.window.bottom_left_frame, row=1, column=0, name="Instructors", width=self.collapsable_width)
-        instructor_scroll = tkh.ScrollFrame(instructor_menu.collapsed_content, width=25, height=self.collapsable_height)
+
+        # add content to the collapsable
+        instructor_scroll = tkh.ScrollFrame(instructor_menu.collapsed_content, width=self.collapsable_width)
 
         self.instructor_vars = {}
         for i, inst in enumerate(self.instructors):
@@ -55,10 +64,8 @@ class Content:
         instructor_scroll.pack(side="top", fill="both", expand=True)
 
     def create_filter_menu(self):
-        
-        self.update_text = tk.Label(self.window.top_frame, text='Last Update:', background="light grey")
-        self.update_text.grid(row=0, column=0, sticky="e")
-        
+
+        #filter variables
         self.filter_vars = {
             "auto_update": tk.BooleanVar(value=1),
             "date_text" : tk.StringVar(),
@@ -68,26 +75,40 @@ class Content:
             "instructor":tk.BooleanVar(value=0)
         }
 
-        # Filter Buttons
-        auto_update_button = tk.Checkbutton(
-            self.window.bottom_right_frame, text="Auto Update", background="White", variable=self.filter_vars["auto_update"], command=self.auto_update)
-        date_label = tk.Label(self.window.bottom_right_frame, text='Date:')
-        date_entry = tk.Entry(self.window.bottom_right_frame, textvariable=self.filter_vars["date_text"])
-        update_button = tk.Button(self.window.bottom_right_frame, text='Update', command=self.on_update_button_press)
-        time_button = tk.Checkbutton(self.window.bottom_right_frame, text="Time", background="White", variable=self.filter_vars["time"])
-        date_button = tk.Checkbutton(self.window.bottom_right_frame, text="Date", background="White", variable=self.filter_vars["date"])
-        room_button = tk.Checkbutton(self.window.bottom_right_frame, text="Room", background="White", variable=self.filter_vars["room"])
-        instructor_button = tk.Checkbutton(self.window.bottom_right_frame, text="Instructor", background="White", variable=self.filter_vars["instructor"])
+        # divide row into two frame (left and right)
+        left_filter_menu = tk.Frame(self.window.bottom_right_frame, bg='white', height=self.filter_bar_height)
+        left_filter_menu.grid(row=0, column=0, sticky="nsew")
+        tk.Grid.columnconfigure(self.window.bottom_right_frame, 0, weight=1)
 
-        # Filter Layout
+        right_filter_menu = tk.Frame(self.window.bottom_right_frame, bg='white', height=self.filter_bar_height)
+        right_filter_menu.grid(row=0, column=1, sticky="nsew")
+        tk.Grid.columnconfigure(self.window.bottom_right_frame, 1, weight=1)
+        right_filter_menu.columnconfigure(0, weight=1) # ensure the first column fills the empty space to the left
+
+        # add widgets
+        auto_update_button = tk.Checkbutton(
+            left_filter_menu, text="Auto Update", background="White", variable=self.filter_vars["auto_update"], command=self.auto_update)
+        date_label = tk.Label(right_filter_menu, text='Date:')
+        date_entry = tk.Entry(right_filter_menu, textvariable=self.filter_vars["date_text"])
+        time_button = tk.Checkbutton(right_filter_menu, text="Time", background="White", variable=self.filter_vars["time"])
+        date_button = tk.Checkbutton(right_filter_menu, text="Date", background="White", variable=self.filter_vars["date"])
+        room_button = tk.Checkbutton(right_filter_menu, text="Room", background="White", variable=self.filter_vars["room"]) 
+        instructor_button = tk.Checkbutton(right_filter_menu, text="Instructor", background="White", variable=self.filter_vars["instructor"])
+        update_button = tk.Button(right_filter_menu, text='Update', command=self.on_update_button_press)
+
+        self.update_text = tk.Label(self.window.top_frame, text='Last Update:', background="light grey")
+
+        # define widget locations
         auto_update_button.grid(row=0, column=0, sticky="nsw")
-        date_label.grid(row=0, column=1, sticky="nsw")
-        date_entry.grid(row=0, column=2, sticky="nsw")
-        time_button.grid(row=0, column=3, sticky="nsew")
-        date_button.grid(row=0, column=4, sticky="nsew")
-        room_button.grid(row=0, column=5, sticky="nsew")
-        instructor_button.grid(row=0, column=6, sticky="nsew")
-        update_button.grid(row=0, column=7, sticky="nsew")
+        date_label.grid(row=0, column=0, sticky="nse")
+        date_entry.grid(row=0, column=1, sticky="nse")
+        time_button.grid(row=0, column=2, sticky="nse")
+        date_button.grid(row=0, column=3, sticky="nse")
+        room_button.grid(row=0, column=4, sticky="nse")
+        instructor_button.grid(row=0, column=5, sticky="nse")
+        update_button.grid(row=0, column=6, sticky="nse")
+
+        self.update_text.grid(row=0, column=0, sticky="nse")
 
     def on_update_button_press(self):
         df = self.df
@@ -131,8 +152,8 @@ class Content:
         self.update_table()
 
     def create_table(self):
-        self.treeview = ttk.Treeview(self.window.bottom_right_frame, show="headings")
-        self.treeview.grid(row=1, column=0, columnspan = 10,sticky="nsew")
+        self.treeview = ttk.Treeview(self.window.bottom_right_frame, show="headings", height = 10)
+        self.treeview.grid(row=1, column=0, columnspan = 2,sticky="nsew")
         columns = self.df.columns.values.tolist()
         sizes = {"Name":95,"Title":160, "Start": 60, "End": 60, "Location": 75, "Instructor / Organization": 300}
         self.treeview["columns"] = columns # set column names
--- tk_helper.py
+++ tk_helper.py
@@ -8,7 +8,7 @@ class ScrollFrame(tk.Frame):
     taken from: 
     https://gist.github.com/mp035/9f2027c3ef9172264532fcd6262f3b01
     """
-    def __init__(self, parent, width=25, height=25):
+    def __init__(self, parent, width=0, height=0):
         super().__init__(parent) # create a frame (self)
 
         self.canvas = tk.Canvas(self, borderwidth=0, background="#ffffff", width=width, height=height)          #place canvas on self
@@ -25,7 +25,7 @@ class ScrollFrame(tk.Frame):
         self.canvas.bind("<Configure>", self.onCanvasConfigure)                       #bind an event whenever the size of the canvas frame changes.
             
         self.viewPort.bind('<Enter>', self.onEnter)                                 # bind wheel events when the cursor enters the control
-        self.viewPort.bind('<Leave>', self.onLeave)                                 # unbind wheel events when the cursorl leaves the control
+        self.viewPort.bind('<Leave>', self.onLeave)                                 # unbind wheel events when the cursor leaves the control
 
         self.onFrameConfigure(None)                                                 #perform an initial stretch on render, otherwise the scroll region has a tiny border until the first resize
 
@@ -56,7 +56,7 @@ class ScrollFrame(tk.Frame):
         else:
             self.canvas.bind_all("<MouseWheel>", self.onMouseWheel)
 
-    def onLeave(self, event):                                                       # unbind wheel events when the cursorl leaves the control
+    def onLeave(self, event):                                                       # unbind wheel events when the cursor leaves the control
         if platform.system() == 'Linux':
             self.canvas.unbind_all("<Button-4>")
             self.canvas.unbind_all("<Button-5>")
@@ -66,32 +66,38 @@ class ScrollFrame(tk.Frame):
 class Collapsable():
     def __init__(self, master, row, column, name, width):
 
-        self.body = tk.Frame(master, bg='purple', width=25, height=50)
+        self.body = tk.Frame(master, bg='purple', width=width)
         self.body.grid(row=row, column=column, sticky="nsew")
-
+        tk.Grid.rowconfigure(master, row, weight=1)
+        
+        self.master = master
+        self.row = row
         self.width = width
         self.name = name
         self.create_button_frame()
         self.create_collapsed_content_frame()
-
-    def create_collapsed_content_frame(self):
-        self.collapsed_content = tk.Frame(self.body, bg='cyan', width=25, height=25)
-        self.collapsed_content.grid(row=1, column=0, sticky="nsew")
 
     def create_button_frame(self):
         self.button_frame = tk.Frame(self.body, bg='grey', width=25, height=25)
         self.button_frame.grid(row=0, column=0, sticky="nsew")
 
         self.button = tk.Button(self.button_frame, text=f'{self.name} \u25BC', width=self.width, command=self.collapse)
-        self.button.grid(row=0, sticky="nsew")        
+        self.button.grid(row=0, sticky="nsew")
+
+    def create_collapsed_content_frame(self):
+        self.collapsed_content = tk.Frame(self.body, bg='cyan')
+        self.collapsed_content.grid(row=1, column=0, sticky="nsew")
+        tk.Grid.rowconfigure(self.body, 1, weight=1)
 
     def collapse(self):
         if self.collapsed_content.winfo_ismapped():
             self.collapsed_content.grid_remove()
-            self.button.config(text=f'{self.name} \u25B6') 
+            self.button.config(text=f'{self.name} \u25B6')
+            tk.Grid.rowconfigure(self.master, self.row, weight=0)
         else:
             self.collapsed_content.grid()
             self.button.config(text=f'{self.name} \u25BC')
+            tk.Grid.rowconfigure(self.master, self.row, weight=1)
 
 class Window:
     def __init__(self, root):
@@ -102,22 +108,23 @@ class Window:
         self.create_bottom_right_frame()
 
     def create_top_frame(self):
-        self.top_frame = tk.Frame(self.root, bg='light grey', width=50, height=25)
+        self.top_frame = tk.Frame(self.root, bg='light grey', height=25)
         self.top_frame.grid(row=0, sticky="nsew")
         tk.Grid.columnconfigure(self.root, 0, weight=1)
+        self.top_frame.columnconfigure(0, weight = 1)
 
     def create_bottom_frame(self):
-        self.bottom_frame = tk.Frame(self.root, bg='green', width=50, height=25)
+        self.bottom_frame = tk.Frame(self.root, bg='green')
         self.bottom_frame.grid(row=1, sticky="nsew")
         tk.Grid.rowconfigure(self.root, 1, weight=1)
     
     def create_bottom_left_frame(self):
-        self.bottom_left_frame = tk.Frame(self.bottom_frame, bg='light grey', width=20, height=190)
+        self.bottom_left_frame = tk.Frame(self.bottom_frame, bg='light grey')
         self.bottom_left_frame.grid(row=0, column=0, sticky="nsew")
         tk.Grid.rowconfigure(self.bottom_frame, 0, weight=1)
 
     def create_bottom_right_frame(self):
-        self.bottom_right_frame = tk.Frame(self.bottom_frame, bg='white', width=250, height=190)
+        self.bottom_right_frame = tk.Frame(self.bottom_frame, bg='white')
         self.bottom_right_frame.grid(row=0, column=1, sticky="nsew")
         tk.Grid.rowconfigure(self.bottom_frame, 0, weight=1)
         tk.Grid.columnconfigure(self.bottom_frame, 1, weight=1)
